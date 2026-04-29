dd7805d800666b0fcbff0c95d591f6470e0ae508
dai <davidngo1704@gmail.com>
2026-04-29 09:45:43 +0700

    sa

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ import { TimelineDemo } from './pages/demo/TimelineDemo';
 import { Aws_Lambda } from './pages/business/Aws_Lambda';
 
 import PrimeReact from 'primereact/api';
-
+import { Crypto_Blockchain } from './pages/business/Crypto_Blockchain';
 
 import { getObjectById } from "./utils/firebase_service"
 
@@ -403,6 +403,25 @@ const App = () => {
                                 <TreeDemo />
                             } 
                         />
+                        <Route 
+                            path="/crypto_blockchain" 
+                            exact 
+                            render={() => 
+                                <Crypto_Blockchain />
+                            } 
+                        />
+
+
+
+
+
+
+
+
+
+
+
+
 
                         <Route 
                             path="/dashboard" 
